2e40799cc02a77fed2d7cbd4e7c32d9d4e5ef0e7
Amala Hema <amalahema@gmail.com>
2023-06-01 22:52:51 -0400

    dist folder created

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,36 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+//putDb and getDb are exported function,content pased as parameter
+  export const putDb = async (content)  => {
+    console.log('PUT to the database');
+  
+    // database connection opened
+    const contactDb = await openDB('jate', 1);
+  
+    // new transaction is created using readwrite  after db conn opened
+    const tx = contactDb.transaction('jate', 'readwrite');
+  
+    // access object store named jate
+    const store = tx.objectStore('jate');
+    //put the provided content into store using put method
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log('Data saved to db', result);
+  };
+  
+  // logic for getMethods
+   export const getDb = async () => {
+    console.log('GET from the database');
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'readonly')
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  };
+  
+  
+  initdb();
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,27 +3,82 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    // Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
+
+      //html webpack plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      // InjectManifest plugin from the workbox-webpack-plugin
+      //InjectManifest plugin, you ensure that the generated service worker is included in your webpack build and can be used in your application.
+      new InjectManifest({
+        swSrc: './src-sw.js',// source file for the service worker
+        swDest: 'src-sw.js',//destination
+      }),
+
+      //When the webpack build runs, the WebpackPwaManifest plugin will generate a web app manifest file based on the provided configuration options. This manifest file includes metadata about your web app, such as its name, description, icons, and other properties
+      // provides metadata and configuration details about the application
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+    
     ],
 
+    //Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
